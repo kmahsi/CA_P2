f49778d2153a0d11a9aef46996a7caba05eabfe9
kamran <kamran2727@gmail.com>
2018-03-28 21:01:20 +0430

    shift rotate module

--- shiftRotate.v
+++ shiftRotate.v
@@ -1,15 +1,24 @@
-module shiftRotate (shiftCount, data, result);
+module shiftRotate (shiftCount, data, operation, result);
 	input [7:0] data;	
 	input [2:0] shiftCount;
 	output reg [7:0] result;
-
-	wire [3:0] finishShifting = shiftCount;
-
+	input [1:0] operation;
 	integer i;
 	always @(*) begin
-		for ( i = 0; i < shiftCount; i = i + 1) begin
-			result <= i;
-		end	
+		result = data;
+			case (operation)
+				2'b00: result <= (result << shiftCount);
+				2'b01: result <= (result >> shiftCount);
+				2'b10:
+					for ( i = 0; i < shiftCount; i = i + 1) begin
+						result = {result[6:0], result[7]};
+					end
+				2'b11: 
+					for ( i = 0; i < shiftCount; i = i + 1) begin
+						result = {result[0], result[7:1]};
+					end
+				default: result <= result;
+			endcase
 	end
 
 endmodule
--- shiftRotate_TB.v
+++ shiftRotate_TB.v
@@ -1,12 +1,29 @@
 module shiftRotateTB();
-	reg [2:0] shiftCnt = 3;
+	reg clk = 0;
+	reg [2:0] shiftCnt = 2;
 	reg [7:0] D = 5;
+	reg [1:0] opt = 2'b00;
 	wire [7:0] rslt;
-
+	initial repeat (10) #50 clk = ~clk;
+	initial begin
+		#40 
+		#20
+		shiftCnt = 3;
+		#40
+		D = 100;
+		opt = 2'b01;
+		#50
+		opt = 2'b10;
+		#30
+		D = 7 + 127;
+		#40
+		opt = 2'b11;
+	end
 
 	shiftRotate shiftR(
 		.shiftCount(shiftCnt), 
 		.data(D), 
+		.operation(opt),
 		.result(rslt)
 	);
 
