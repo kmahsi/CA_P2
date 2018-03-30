c5ec62ed321ac34f3b24d9860e0a7f5f17a41659
SaraSaffari <sarasaffari.1997@gmail.com>
2018-03-30 23:57:24 +0430

    controller is half way through

--- controller.v
+++ controller.v
@@ -1,35 +1,67 @@
-module controller (clock, rst, zero, carryOut, lasttwoBits, lastthreeBits, twoBitFn, threeBitFn, 
-	selectToWrite, selectRd, selectAluArg, ALUfunction, sh_roFunction, STM, LDM, rstPC, enablePC);
-	input clock, rst, zero, carryOut;
+module controller (clock, rst, lasttwoBits, lastthreeBits, twoBitFn, threeBitFn, 
+	selectToWrite, selectRd, selectAluArg, ALUfunction, sh_roFunction, STM, LDM,
+	rstPC, enablePC, rstZero, enableZero, rstCarry, enableCarry, memRead, memWrite);
+
+	input clock, rst;
 	input [1:0]lasttwoBits, twoBitFn; 
 	input  [2:0]lastthreeBits, threeBitFn;
-	output reg selectToWrite, selectRd, selectAluArg, STM, LDM, rstPC, enablePC;
+	output reg selectRd, STM, LDM,
+	 rstPC, enablePC, rstZero, enableZero, rstCarry, enableCarry, memRead, memWrite;
+	output reg[1:0]selectAluArg, selectToWrite;
 	output reg[2:0]ALUfunction;
 	output reg[1:0]sh_roFunction;
+	reg[1:0] memFunction;
 	
 	always @(posedge clock)
 		enablePC <= 1'b1;
 
 	always @(*) begin
-			case(lasttwoBits)
-				2'b 00 : ALUfunction <= threeBitFn;
-				2'b 01 : selectAluArg <= 1'b0; // with 0 signal the mux choses const number
-			endcase
+		case(lasttwoBits)
+			2'b 00 : begin 
+				ALUfunction <= threeBitFn;
+				selectAluArg <= lasttwoBits;
+				selectToWrite <= 2'b00; // with 00 signal the mux choses result of ALU
+				enableCarry= 1'b1;
+				enableZero= 1'b1;
+				end
+			2'b 01 : begin 
+				ALUfunction <= threeBitFn;
+				selectAluArg <= lasttwoBits; 
+				selectToWrite <= 2'b01; // with 01 signal the mux choses result of immediate  
+				enableCarry = 1'b1;
+				enableZero = 1'b1;
+				end
+		endcase
 	end
 
 	always @(*) begin
-			case(lastthreeBits)
-				3'b 110: sh_roFunction <= twoBitFn;
-				// 3'b 100: 
-				// 3'b 101:  conditional jump
-			endcase
-	end	
-// these lines had error what says Illegal reference to net variable , 
-//which is actually true because zero and carryout are inputs in this code.
-	// always @(posedge clock)
-	// 	if(rst) begin 
-	// 		zero <= 1'b0;
-	// 		carryOut <= 1'b0;
-	// 		rstPC <= 1'b1;
-	// 	end 
+		case(lastthreeBits)
+			3'b 110: begin
+				sh_roFunction <= twoBitFn;
+				selectToWrite <= 2'b10; // with 10 signal the mux choses result of shift_rotate
+
+
+				// shift and rotate does not have enable but in picture there is a line for it !!!!!!
+				end
+			3'b 100: begin 
+				if(twoBitFn == 2'b00) begin
+					LDM <=1'b1;
+					memRead <= 1'b1;
+				end
+
+				if(twoBitFn == 2'b01) begin	
+					STM <=1'b1;
+					memWrite <= 1'b1;
+				end
+			end 
+		endcase
+	end
+
+	always @(posedge clock)
+		if(rst) begin 
+			rstZero <= 1'b1;
+			rstCarry <= 1'b1;
+			rstPC <= 1'b1;
+		end 
+
 endmodule
