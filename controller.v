c6ea736139becbe2111afbbf30dcaf54aaa6a846
SaraSaffari <sarasaffari.1997@gmail.com>
2018-04-01 14:31:38 +0430

    final

--- controller.v
+++ controller.v
@@ -1,11 +1,12 @@
-module controller (clock, allBits, selectToWrite, selectR2, selectAluArg, ALUfunction, sh_roFunction,
-	STM, LDM, enablePC, enableZero, enableCarry, memRead);
+module controller (clock, allBits, Zero, CarryOut, selectToWrite, selectR2, selectAluArg, ALUfunction, sh_roFunction,
+	STM, LDM, enablePC, enableZero, enableCarry, memRead,selectAdress, push, pop, RET);
 
 	input clock;
 	input[18:0]allBits;
+	input Zero, CarryOut;
 
-	output reg selectR2, selectAluArg, STM, LDM, enablePC, enableZero, enableCarry, memRead;
-	output reg[1:0]selectToWrite;
+	output reg selectR2, selectAluArg, STM, LDM, enablePC, enableZero, enableCarry, memRead, push, pop, RET;
+	output reg[1:0]selectToWrite, selectAdress;
 	output reg[2:0]ALUfunction;
 	output reg[1:0]sh_roFunction;
 	
@@ -24,10 +25,19 @@ module controller (clock, allBits, selectToWrite, selectR2, selectAluArg, ALUfun
 	assign lastthreeBits = allBits[18:16];
 	wire[1:0]twoBitFn;
 	assign twoBitFn = allBits[15:14];
-
+	wire[4:0]lastfiveBits;
+	assign lastfiveBits = allBits[18:14];
+	wire[11:0]Adress;
+	assign Adress = allBits[11:0];
+	wire[5:0]lastsixBits;
+	assign lastsixBits = allBits[18:13];
 
 	always @(*) begin
-		LDM <= 0; STM <= 0; memRead <= 1'b0;
+		LDM <= 1'b0; STM <= 1'b0; memRead <= 1'b0;
+		enableCarry <= 1'b0;
+		enableZero <= 1'b0;
+		push <= 1'b0; pop <= 1'b0; RET <= 1'b0;
+
 		case(lasttwoBits)
 			2'b 00 : begin 
 				LDM <= 1'b1;
@@ -53,8 +63,6 @@ module controller (clock, allBits, selectToWrite, selectR2, selectAluArg, ALUfun
 			3'b 110: begin
 				sh_roFunction <= twoBitFn;
 				selectToWrite <= 2'b01; // with 01 signal the mux choses result of shift_rotate
-				enableCarry = 1'b0;
-				enableZero = 1'b0;
 				LDM <= 1'b1;
 				end
 			3'b 100: begin 
@@ -62,18 +70,52 @@ module controller (clock, allBits, selectToWrite, selectR2, selectAluArg, ALUfun
 					LDM <= 1'b1;
 					memRead <= 1'b1;
 					selectToWrite <= 2'b10; // with 01 signal the mux choses result of dataMemory
-					enableCarry = 1'b0;
-					enableZero = 1'b0;
 				end
 
 				if(twoBitFn == 2'b01) begin	
 					STM <= 1'b1;
 					selectR2 <= 1'b0; // with 0 signal the mux choses[13:11]
-					enableCarry = 1'b0;
-					enableZero = 1'b0;
 				end
 			end 
 		endcase
+
+		case(lastthreeBits)
+			3'b 101: begin
+				if ({twoBitFn , Zero} == 3'b 001) selectAdress <= 2'b00;
+
+				if ({twoBitFn , Zero} == 3'b 000) selectAdress <= 2'b01;
+
+				if ({twoBitFn , Zero} == 3'b 010) selectAdress <= 2'b00;
+
+				if ({twoBitFn , Zero} == 3'b 011) selectAdress <= 2'b01;
+
+				if ({twoBitFn , CarryOut} == 3'b 101) selectAdress <= 2'b00;
+
+				if ({twoBitFn , CarryOut} == 3'b 100) selectAdress <= 2'b01;
+
+				if ({twoBitFn , CarryOut} == 3'b 110) selectAdress <= 2'b00;
+
+				if ({twoBitFn , CarryOut} == 3'b 111) selectAdress <= 2'b01;
+			end
+		endcase 
+
+		case(lastfiveBits)
+			5'b 11100: 
+				selectAdress <= 2'b10;
+
+			5'b 11101: begin
+				selectAdress <= 2'b10;
+				push <= 1'b1;
+			end
+		endcase 
+
+		case(lastsixBits)
+			6'b 111100: begin
+				pop <= 1'b1;
+				RET <= 1'b1;
+			end
+		endcase
+
 	end
 
 endmodule
